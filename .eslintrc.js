937d72bced95b14df0ee7b658654634613f94064
Trong <trongnguyen@x3english.com>
2019-07-19 17:10:40 +0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['vue'], // enable vue plugin
+  extends: ['plugin:vue/essential', 'prettier'], // activate vue related rules,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: false
+  }
+}
