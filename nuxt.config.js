f4142f12821cbe8ef843a036cd7d1791f4e84ba9
root <root@Trong-Centos6.9>
2019-07-19 03:27:02 -0700

    hello

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  server: {
+    port: 8001, // default: 3000
+  },
   /*
   ** Customize the progress-bar color
   */
